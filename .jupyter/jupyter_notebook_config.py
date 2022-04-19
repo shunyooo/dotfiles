b91655942e70fda6b33e44bfe86e360c7a5c44b8
shunyooo <shunyo.kawamoto@gmail.com>
2022-04-19 13:38:26 +0900

    add jupyter settings

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,28 @@
+import multiprocessing
+
+import psutil
+
+c.ServerApp.token = ''
+c.ServerApp.terminado_settings = { 'shell_command': ['/bin/bash'] }
+
+c.JupyterLabTemplates.template_dirs = ['list', 'of', 'template', 'directories']
+c.JupyterLabTemplates.include_default = True
+c.JupyterLabTemplates.include_core_paths = True
+
+# memory
+mem = psutil.virtual_memory() 
+c.ResourceUseDisplay.mem_limit = mem.total
+
+# cpu
+c.ResourceUseDisplay.track_cpu_percent = True
+c.ResourceUseDisplay.cpu_limit = multiprocessing.cpu_count()
+
+# for @krassowski/jupyterlab-lsp
+# https://jupyterlab-lsp.readthedocs.io/en/latest/Configuring.html
+# c.LanguageServerManager.language_servers = {
+#     "python": {
+#         "argv": ["python", "-m", "pyls"],
+#         "languages": ["python"],
+#         "version": 2
+#     }
+# }
